b5209624e2298bc7e7aba3e9a1825a151d341d13
derek <adamtsai0408@gmail.com>
2026-04-29 20:30:22 +0800

    fix: 更改bingobingo呼叫參數時間

--- backend/app/service/cronJob.py
+++ backend/app/service/cronJob.py
@@ -65,7 +65,7 @@ scheduler.add_job(
       "cron",
       day_of_week="mon-sun",
       hour="7-23",
-      minute="*/5", 
+      minute="2/5", 
   )
 scheduler.add_job(run_games_job, "cron", day_of_week="mon-sat", hour=21, minute=30)
 
